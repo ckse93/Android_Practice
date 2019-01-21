c00b6feaf742cbe68a8c658526d8619d72a7a32a
Chan Jung <ckse93@gmail.com>
2019-01-20 21:06:59 -0600

    Create MainActivity.java

---
+++ TextField_login/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.android.demo1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    public int count = 0;
+
+    public void onCLick (View view){
+        EditText mytextField = findViewById(R.id.myTextField);
+        count++;
+        Log.i("Info", "Button pressed for the " + count + " th time\n" + mytextField.getText()); // displaying information on "run" screen on android studio
+
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
