5559f24ddd61d58e1ff13405a9dd2535e6c600b3
Chan Jung <ckse93@gmail.com>
2019-01-21 21:22:11 -0600

    Create MainActivity.java

---
+++ currency_converter_USDtoKRW/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.android.demo1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    public int count = 0;
+
+    public void onClick (View view){
+        EditText numText = findViewById(R.id.numText);
+        Double dollars = Double.parseDouble(numText.getText().toString()); // uppper case D because we are using class Double.
+        double converted = dollars *1100.3;
+        Toast.makeText(this, converted + "KRW", Toast.LENGTH_SHORT).show();
+
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
