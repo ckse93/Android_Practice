89e210926570d3260b40e192148a713b583e2494
Chan Jung <ckse93@gmail.com>
2019-02-12 11:01:59 -0600

    Create for_loop.java

---
+++ for_loop.java
@@ -0,0 +1,21 @@
+import java.util.* ;
+
+public class mainnn {
+	public static void main(String[] args){
+    String[] family_array = {"Chan", "Kendra", "Yoonmo", "Jongsuk", "Bill"};
+    for (String array_name : family_array){
+    System.out.println(array_name);
+    }
+    
+		List<String> family = new ArrayList<String>();
+		family.add("Chan");
+		family.add("Kendra");
+		family.add("Yoonmo");
+		family.add("Jongsuk");
+		family.add("Bill");
+		
+		for (String names : family){
+		System.out.println(names);
+		}
+	}
+}
