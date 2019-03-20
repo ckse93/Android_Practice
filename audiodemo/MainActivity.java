d2dea32ffe900df8b6bb8d53bcf57e2af0e865dc
Chan Jung <ckse93@gmail.com>
2019-03-20 17:02:13 -0500

    Update MainActivity.java

--- audiodemo/MainActivity.java
+++ audiodemo/MainActivity.java
@@ -3,19 +3,25 @@ package com.example.android.videodemo;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.MediaController;
 import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
+    MediaPlayer mp;                 // 1. make an "empty" variable to declare it
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // literally two lines of code
-        MediaPlayer mp = MediaPlayer.create(this, R.raw.scarface); 
-        // creating MediaPlayer, and linking context with the res/raw/scarface.mp3 file 
-        
-        mp.start();
+        mp = MediaPlayer.create(this, R.raw.scarface); // 2. this will instantiate the mp variable for later use
+    }
+
+    public void play (View v){
+        mp.start();     // since the variable is already declared, it can use it however you want
+    }
+
+    public void pause (View v){
+        mp.pause();
     }
 }
