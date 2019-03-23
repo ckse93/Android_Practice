2c27805de49ece59c30621ae780258ef6645198c
Chan Jung <ckse93@gmail.com>
2019-03-23 15:54:12 -0500

    Update MainActivity.java

--- ListViewDemo/MainActivity.java
+++ ListViewDemo/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+/*
         ListView myListView = findViewById(R.id.myListView);
 
         final ArrayList<String > myFamily = new ArrayList<String>();
@@ -42,6 +42,40 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+    */
+        
+        
+        /*
+        * 1. make a list of thing you want to display (ArrayList)
+        * 2. make an adapter to link the arrayList and the context
+        * 3. link your ListView with the arrayAdapter
+        * 4. make a method for that list to what to do when clicked. 
+        * */
+
+        ListView myListView = findViewById(R.id.myListView);
+
+        final ArrayList<String> friendList = new ArrayList<>();
+        friendList.add("Jaci");
+        friendList.add("Genny");
+        friendList.add("WOnbin");
+        friendList.add("Jeonghoon");
+        friendList.add("Tom");
+        friendList.add("Isai");
+        friendList.add("Ashimita");
+        friendList.add("Will");
+        friendList.add("Cassie");
+        friendList.add("Tony");
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1, friendList);
+
+        myListView.setAdapter(arrayAdapter);
+        myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String DisplayName = friendList.get(i).toString();
+                Toast.makeText(MainActivity.this, "Person Selected : " + DisplayName, Toast.LENGTH_SHORT).show();
+            }
+        });
 
 
 }
